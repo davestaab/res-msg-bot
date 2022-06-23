355214f9276c45865b641217bab75ee109d99224
Dave Staab <davestaab@gmail.com>
2022-06-22 21:53:05 -0400

    add new tva images 👌

--- netlify/functions/tva.ts
+++ netlify/functions/tva.ts
@@ -1,12 +1,25 @@
-import { Handler } from "@netlify/functions";
-import { POST as DiscordPost } from "../types/DiscordPost"
+import {Handler} from "@netlify/functions";
+import {POST as DiscordPost} from "../types/DiscordPost"
 import fetch from "node-fetch";
 import format from 'date-fns/format';
 
 const images = [
-  "https://res-msg-bot.netlify.app/images/Miss_minutes.png",
-  "https://res-msg-bot.netlify.app/images/tva-modius.jpg",
-  "https://res-msg-bot.netlify.app/images/tva-leader.png"
+  "https://res-msg-bot.netlify.app/images/TVA-A-01.png",
+  "https://res-msg-bot.netlify.app/images/TVA-A-02.png",
+  "https://res-msg-bot.netlify.app/images/TVA-A-03.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-02.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-03.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-04.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-05.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-06.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-07.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-08.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-10.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-11.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-12.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-13.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-14.jpg",
+  "https://res-msg-bot.netlify.app/images/TVA-15.jpg",
 ];
 
 const handler: Handler = async () => {
@@ -16,7 +29,7 @@ const handler: Handler = async () => {
     embeds: [
       {
         title: "Don't forget....",
-        description:  "Just a friendly reminder from the Time Variance Authority: " +
+        description: "Just a friendly reminder from the Time Variance Authority: " +
           "Avoid a nexus event and enter your time! We can all do our" +
           " part to protect and preserve the Sacred Timeline.\n\n" +
           "-- The Time-Keepers",
@@ -36,15 +49,15 @@ const handler: Handler = async () => {
   };
 }
 
-export { handler };
+export {handler};
 
 function randomImage() {
-  return images[getRandomArbitrary(0,images.length)];
+  return images[getRandomArbitrary(0, images.length)];
 }
 
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
-function getRandomArbitrary(min:number, max:number) {
+function getRandomArbitrary(min: number, max: number) {
   return Math.floor(Math.random() * (max - min) + min);
 }
