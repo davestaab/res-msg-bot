e1a5d271b14b6031885d0c9f940f2383219f6494
Dave Staab <davestaab@gmail.com>
2022-06-27 22:01:34 -0400

    linted the lint config ➰

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
     '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   rules: {
     'prettier/prettier': [
@@ -20,12 +20,15 @@ module.exports = {
         tabWidth: 2,
         singleQuote: true,
         printWidth: 100,
-        endOfLine: 'auto'
-      }
+        endOfLine: 'auto',
+      },
     ],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-use-before-define': ['error', 'nofunc'],
-    '@typescript-eslint/no-unused-vars': ['warn', {ignoreRestSiblings: true,  varsIgnorePattern: "_.*"}],
-    '@typescript-eslint/no-floating-promises': ['error']
-  }
-}
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { ignoreRestSiblings: true, varsIgnorePattern: '_.*' },
+    ],
+    '@typescript-eslint/no-floating-promises': ['error'],
+  },
+};
