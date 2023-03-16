9e1301bdfde948d2c3847a32400eac3bb8e80907
Staab, Dave <i47@ornl.gov>
2023-03-16 14:01:56 -0400

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,26 +3,27 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    // 'plugin:vue/essential',
     '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
+    // '@vue/eslint-config-prettier',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
     project: './tsconfig.json',
+    extraFileExtensions: ['.cjs']
   },
   rules: {
-    'prettier/prettier': [
-      'warn',
-      {
-        tabWidth: 2,
-        singleQuote: true,
-        printWidth: 100,
-        endOfLine: 'auto',
-      },
-    ],
+    // 'prettier/prettier': [
+    //   'warn',
+    //   {
+    //     tabWidth: 2,
+    //     singleQuote: true,
+    //     printWidth: 100,
+    //     endOfLine: 'auto',
+    //   },
+    // ],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-use-before-define': ['error', 'nofunc'],
     '@typescript-eslint/no-unused-vars': [
